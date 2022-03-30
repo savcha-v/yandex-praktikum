e78d4a977035221ad985cc8d1737a840756fbb5b
savcha-v <dev.savcha@gmail.com>
2022-03-30 13:02:18 +0300

    inc3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/go-chi/chi/v5"
 )
 
 var Urls map[string]string = map[string]string{
@@ -13,69 +15,74 @@ var Urls map[string]string = map[string]string{
 }
 var Unic int
 
-// Short — обработчик запросов.
-func Short(w http.ResponseWriter, r *http.Request) {
-
-	switch r.Method {
-	case http.MethodPost:
-
-		body, err := io.ReadAll(r.Body)
-		defer r.Body.Close()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		urlToShort := string(body)
-		if urlToShort == "" {
-			http.Error(w, "Shortcut url not found", http.StatusBadRequest)
-			return
-		}
-
-		Unic += 1
-		UnicStr := strconv.Itoa(Unic)
-		Urls[UnicStr] = urlToShort
-
-		responseURL := "http://" + r.Host + r.URL.String() + "?id=" + UnicStr
-
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.WriteHeader(http.StatusCreated)
-		w.Write([]byte(responseURL))
-		fmt.Fprint(w)
-
-	case http.MethodGet:
-
-		id := r.URL.Query().Get("id")
-		if id == "" {
-			http.Error(w, "'id' missing", http.StatusBadRequest)
-			return
-		}
-
-		reternURL, exists := Urls[id]
-		if !exists {
-			http.Error(w, "'id' not found", http.StatusBadRequest)
-			return
-		}
-
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.Header().Add("Location", reternURL)
-		w.WriteHeader(http.StatusTemporaryRedirect)
-		w.Write([]byte(""))
-		fmt.Fprint(w)
-
-	default:
-
-		http.Error(w, "POST or GET requests are allowed!", http.StatusMethodNotAllowed)
+func PostShort(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "POST requests are allowed!", http.StatusMethodNotAllowed)
 		return
 	}
+
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	urlToShort := string(body)
+	if urlToShort == "" {
+		http.Error(w, "Shortcut url not found", http.StatusBadRequest)
+		return
+	}
+
+	Unic += 1
+	UnicStr := strconv.Itoa(Unic)
+	Urls[UnicStr] = urlToShort
+
+	responseURL := "http://" + r.Host + r.URL.String() + "?id=" + UnicStr
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(responseURL))
+	fmt.Fprint(w)
+}
+
+func GetShort(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		http.Error(w, "GET requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, "'id' missing", http.StatusBadRequest)
+		return
+	}
+
+	reternURL, exists := Urls[id]
+	if !exists {
+		http.Error(w, "'id' not found", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Location", reternURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+	w.Write([]byte(""))
+	fmt.Fprint(w)
+
 }
 
 func main() {
 
-	http.HandleFunc("/", Short)
+	r := chi.NewRouter()
+	r.Route("/", func(r chi.Router) {
+		r.Get("/", GetShort)
+		r.Post("/", PostShort)
+	})
 
 	delete(Urls, "0")
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 
 }
--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -8,122 +8,158 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestShort(t *testing.T) {
-	type want struct {
-		body   string
-		status int
+type Want struct {
+	body   string
+	status int
+}
+type TestStruct struct {
+	name    string
+	request string
+	method  string
+	body    io.Reader
+	want    Want
+}
+
+var TestsForAll = []TestStruct{
+	{
+		name:    "test endpoint DELETE",
+		method:  http.MethodDelete,
+		request: "/?id=1",
+		want: Want{
+			status: http.StatusMethodNotAllowed,
+			body:   "POST or GET requests are allowed!\n",
+		},
+	},
+	{
+		name:    "test endpoint PUT",
+		method:  http.MethodPut,
+		request: "/?id=1",
+		want: Want{
+			status: http.StatusMethodNotAllowed,
+			body:   "POST or GET requests are allowed!\n",
+		},
+	},
+	{
+		name:    "test endpoint PATCH",
+		method:  http.MethodPatch,
+		request: "/?id=1",
+		want: Want{
+			status: http.StatusMethodNotAllowed,
+			body:   "POST or GET requests are allowed!\n",
+		},
+	},
+	{
+		name:    "test endpoint CONNECT",
+		method:  http.MethodConnect,
+		request: "/?id=1",
+		want: Want{
+			status: http.StatusMethodNotAllowed,
+			body:   "POST or GET requests are allowed!\n",
+		},
+	},
+	{
+		name:    "test endpoint HEAD",
+		method:  http.MethodHead,
+		request: "/?id=1",
+		want: Want{
+			status: http.StatusMethodNotAllowed,
+			body:   "POST or GET requests are allowed!\n",
+		},
+	},
+	{
+		name:    "test endpoint OPTIONS",
+		method:  http.MethodOptions,
+		request: "/?id=1",
+		want: Want{
+			status: http.StatusMethodNotAllowed,
+			body:   "POST or GET requests are allowed!\n",
+		},
+	},
+	{
+		name:    "test endpoint TRACE",
+		method:  http.MethodTrace,
+		request: "/?id=1",
+		want: Want{
+			status: http.StatusMethodNotAllowed,
+			body:   "POST or GET requests are allowed!\n",
+		},
+	},
+}
+
+var TestsForPost = []TestStruct{
+	{
+		name:    "test endpoint POST",
+		method:  http.MethodPost,
+		request: "/",
+		body:    strings.NewReader("https://quickref.me/golang"),
+		want: Want{
+			body:   "http://example.com/?id=1",
+			status: http.StatusCreated,
+		},
+	},
+}
+
+var TestsForGet = []TestStruct{
+	{
+		name:    "test endpoint GET",
+		method:  http.MethodGet,
+		request: "/?id=1",
+		want: Want{
+			status: http.StatusBadRequest,
+			body:   "'id' not found\n",
+		},
+	},
+}
+
+func changeResult(t *testing.T, result *http.Response, want Want) {
+
+	require.Equal(t, want.status, result.StatusCode)
+
+	_, err := ioutil.ReadAll(result.Body)
+	require.NoError(t, err)
+	err = result.Body.Close()
+	require.NoError(t, err)
+}
+
+func TestGetShort(t *testing.T) {
+	tests := [][]TestStruct{
+		TestsForAll,
+		TestsForGet,
 	}
-	tests := []struct {
-		name    string
-		request string
-		method  string
-		body    io.Reader
-		want    want
-	}{
-		{
-			name:    "test endpoint GET",
-			method:  http.MethodGet,
-			request: "/?id=1",
-			want: want{
-				status: http.StatusBadRequest,
-				body:   "'id' not found\n",
-			},
-		},
-		{
-			name:    "test endpoint DELETE",
-			method:  http.MethodDelete,
-			request: "/?id=1",
-			want: want{
-				status: http.StatusMethodNotAllowed,
-				body:   "POST or GET requests are allowed!\n",
-			},
-		},
-		{
-			name:    "test endpoint PUT",
-			method:  http.MethodPut,
-			request: "/?id=1",
-			want: want{
-				status: http.StatusMethodNotAllowed,
-				body:   "POST or GET requests are allowed!\n",
-			},
-		},
-		{
-			name:    "test endpoint PATCH",
-			method:  http.MethodPatch,
-			request: "/?id=1",
-			want: want{
-				status: http.StatusMethodNotAllowed,
-				body:   "POST or GET requests are allowed!\n",
-			},
-		},
-		{
-			name:    "test endpoint CONNECT",
-			method:  http.MethodConnect,
-			request: "/?id=1",
-			want: want{
-				status: http.StatusMethodNotAllowed,
-				body:   "POST or GET requests are allowed!\n",
-			},
-		},
-		{
-			name:    "test endpoint HEAD",
-			method:  http.MethodHead,
-			request: "/?id=1",
-			want: want{
-				status: http.StatusMethodNotAllowed,
-				body:   "POST or GET requests are allowed!\n",
-			},
-		},
-		{
-			name:    "test endpoint OPTIONS",
-			method:  http.MethodOptions,
-			request: "/?id=1",
-			want: want{
-				status: http.StatusMethodNotAllowed,
-				body:   "POST or GET requests are allowed!\n",
-			},
-		},
-		{
-			name:    "test endpoint TRACE",
-			method:  http.MethodTrace,
-			request: "/?id=1",
-			want: want{
-				status: http.StatusMethodNotAllowed,
-				body:   "POST or GET requests are allowed!\n",
-			},
-		},
-		{
-			name:    "test endpoint POST",
-			method:  http.MethodPost,
-			request: "/",
-			body:    strings.NewReader("https://quickref.me/golang"),
-			want: want{
-				body:   "http://example.com/?id=1",
-				status: http.StatusCreated,
-			},
-		},
+	for _, maptt := range tests {
+		for _, tt := range maptt {
+			t.Run(tt.name, func(t *testing.T) {
+				request := httptest.NewRequest(tt.method, tt.request, tt.body)
+				w := httptest.NewRecorder()
+				h := http.HandlerFunc(GetShort)
+				h.ServeHTTP(w, request)
+				result := w.Result()
+				changeResult(t, result, tt.want)
+			})
+		}
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			request := httptest.NewRequest(tt.method, tt.request, tt.body)
-			w := httptest.NewRecorder()
-			h := http.HandlerFunc(Short)
-			h.ServeHTTP(w, request)
-			result := w.Result()
 
-			require.Equal(t, tt.want.status, result.StatusCode)
+}
 
-			bodyResult, err := ioutil.ReadAll(result.Body)
-			require.NoError(t, err)
-			err = result.Body.Close()
-			require.NoError(t, err)
+func TestPostShort(t *testing.T) {
 
-			assert.Equal(t, tt.want.body, string(bodyResult))
-
-		})
+	tests := [][]TestStruct{
+		TestsForAll,
+		TestsForPost,
+	}
+	for _, maptt := range tests {
+		for _, tt := range maptt {
+			t.Run(tt.name, func(t *testing.T) {
+				request := httptest.NewRequest(tt.method, tt.request, tt.body)
+				w := httptest.NewRecorder()
+				h := http.HandlerFunc(PostShort)
+				h.ServeHTTP(w, request)
+				result := w.Result()
+				changeResult(t, result, tt.want)
+			})
+		}
 	}
 }
