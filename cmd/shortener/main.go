d08ecd1a82d1ea1e21f4a43d461ac948e7a64423
savcha-v <dev.savcha@gmail.com>
2022-04-06 06:55:23 +0300

    inc5

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,18 +5,30 @@ import (
 	"net/http"
 	handlers "yandex-praktikum/internal/handlers"
 
+	"github.com/caarlos0/env/v6"
 	"github.com/go-chi/chi/v5"
 )
 
+type Config struct {
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	BaseURL       string `env:"BASE_URL"`
+}
+
 func main() {
+
+	var cfg Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	r := chi.NewRouter()
 	r.Route("/", func(r chi.Router) {
-		r.Get("/", handlers.GetShort)
+		r.Get("/"+cfg.BaseURL+"/", handlers.GetShort)
 		r.Post("/", handlers.PostShort)
 		r.Post("/api/shorten", handlers.PostShorten)
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(cfg.ServerAddress+":8080", r))
 
 }
