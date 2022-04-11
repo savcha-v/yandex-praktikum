cd29ab52bd0156b1198986ab35a0246d06be9743
savcha-v <dev.savcha@gmail.com>
2022-04-11 07:29:43 +0300

    inc6

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/caarlos0/env v3.5.0+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
--- internal/store/urls.go
+++ internal/store/urls.go
@@ -1,32 +1,112 @@
 package store
 
 import (
+	"bufio"
+	"encoding/json"
+	"log"
+	"os"
 	"strconv"
+
+	"github.com/caarlos0/env"
 )
 
-var urls = make(map[int]string)
+var FileName string
+
+type UnitURL struct {
+	Full  string `json:"full"`
+	Short string `json:"short"`
+}
+
+var urls = make(map[int]UnitURL)
+
+type Config struct {
+	FileStor string `env:"FILE_STORAGE_PATH" envDefault:""`
+}
+
+func InitStorage() {
+
+	var cfg Config
+	if err := env.Parse(&cfg); err != nil {
+		log.Fatal(err)
+	}
+	FileName = cfg.FileStor
+	if FileName != "" {
+
+		file, err := os.OpenFile(FileName, os.O_RDONLY|os.O_CREATE, 0777)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan() {
+
+			if scanner.Err() != nil {
+				log.Fatal(scanner.Err())
+			}
+			data := scanner.Bytes()
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			json.Unmarshal([]byte(data), &urls)
+
+		}
+	}
+}
 
 func GetID(urlToShort string) string {
 
 	nextID := getNextID()
-	urls[nextID] = urlToShort
+	until := UnitURL{
+		Full:  urlToShort,
+		Short: "fff",
+	}
+	urls[nextID] = until
+	//записать в файл
+	if FileName != "" {
+
+		file, err := os.OpenFile(FileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		var record = make(map[int]UnitURL)
+
+		record[nextID] = until
+		data, err := json.Marshal(record)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		writer := bufio.NewWriter(file)
+		if _, err := writer.Write(data); err != nil {
+			log.Fatal(err)
+		}
+		if err := writer.WriteByte('\n'); err != nil {
+			log.Fatal(err)
+		}
+		writer.Flush()
+	}
 	return strconv.Itoa(nextID)
 
 }
 
-func GetURL(id string) (url string, strErr string) {
+func GetURL(idStr string) (url string, strErr string) {
 
-	idStr, err := strconv.Atoi(id)
+	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		return "", "'id' not found"
 	}
 
-	url, exists := urls[idStr]
+	until, exists := urls[id]
 	if !exists {
 		return "", "'id' not found"
 	}
 
-	return url, ""
+	return until.Full, ""
 }
 
 func getNextID() int {
